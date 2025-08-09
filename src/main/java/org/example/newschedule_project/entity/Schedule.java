e178939fecbe00d6050ab8eec155d83d7d0b5e63
AllSungho <nanumeran@gmail.com>
2025-08-09 15:47:04 +0900

    Create schedule entity

---
+++ src/main/java/org/example/newschedule_project/entity/Schedule.java
@@ -0,0 +1,26 @@
+package org.example.newschedule_project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Schedule extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String title;
+    private String content;
+
+    public Schedule(String name, String title, String content) {
+        this.name = name;
+        this.title = title;
+        this.content = content;
+    }
+}
