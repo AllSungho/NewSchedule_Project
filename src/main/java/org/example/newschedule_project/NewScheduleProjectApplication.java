8d11def0d8f55cc538e33fb6a8da9661446bea40
AllSungho <nanumeran@gmail.com>
2025-08-09 15:07:10 +0900

    first commit

---
+++ src/main/java/org/example/newschedule_project/NewScheduleProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.newschedule_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewScheduleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewScheduleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/newschedule_project/NewScheduleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newschedule_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewScheduleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
