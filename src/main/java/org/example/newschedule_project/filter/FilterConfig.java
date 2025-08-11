8510cbff455b67bc1dc07a3e97195062b9bce771
AllSungho <nanumeran@gmail.com>
2025-08-11 22:49:19 +0900

    Catch error

--- src/main/java/org/example/newschedule_project/filter/FilterConfig.java
+++ src/main/java/org/example/newschedule_project/filter/FilterConfig.java
@@ -1,11 +1,11 @@
 package org.example.newschedule_project.filter;
 
 import jakarta.servlet.Filter;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable
+@Configuration
 public class FilterConfig {
 
     @Bean
